44f50742eb63548712a3da34f8d85510b7f29b04
Eduardo <eduardo.conti@gazin.com.br>
2022-11-03 13:19:34 -0300

    refact: alter colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import ClippedDrawer from "./components/drawer";
-
+const theme = createTheme({});
 function App() {
-  return <ClippedDrawer />;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <ClippedDrawer />
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/components/drawer.tsx
+++ src/components/drawer.tsx
@@ -7,23 +7,21 @@ import React from "react";
 import {
   Button,
   CircularProgress,
+  Divider,
   Grid,
   IconButton,
   Snackbar,
   Typography,
+  useTheme,
 } from "@mui/material";
 import JsonFormatter from "react-json-formatter";
 import { v4 as uuidv4 } from "uuid";
 import io from "socket.io-client";
-import ShareIcon from "@mui/icons-material/Share";
+import { lightGreen, orange } from "@mui/material/colors";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 
+const MAX_REQUESTS = 30;
 const drawerWidth = 240;
-
-const jsonStyle = {
-  propertyStyle: { color: "red" },
-  stringStyle: { color: "green" },
-  numberStyle: { color: "darkorange" },
-};
 
 export type Requests = {
   body: string;
@@ -38,28 +36,34 @@ const apiHost = process.env.REACT_APP_API_HOST as string;
 export default function ClippedDrawer() {
   const [requests, setRequests] = React.useState<Requests[]>([]);
   const [selectedRequestId, setSelectedRequestId] = React.useState<string>("");
-  console.log('init')
+  const theme = useTheme();
+  const jsonStyle = {
+    propertyStyle: { color: orange[900] },
+    stringStyle: { color: theme.palette.primary.main },
+    numberStyle: { color: lightGreen[900] },
+  };
+
   React.useEffect(() => {
-    console.log('effect')
     const socket = io(apiHost);
     socket.on("disconnect", (reason) => {
-      console.log(reason)
-      if (reason === "io server disconnect") {
-        // the disconnection was initiated by the server, you need to manually reconnect
-        console.log(socket.active); // false
-      }
-      socket.off(webHookId)
+      console.log("io server disconnect. Reason: " + reason);
+      socket.off(webHookId);
     });
+
     socket.on("connect", () => {
       console.log("connected " + webHookId);
       socket.on(webHookId, (content: any) => {
-        setRequests((req) => [content, ...req]);
+        setRequests((req) => {
+          if (req.length >= MAX_REQUESTS) return [content];
+          return [content, ...req];
+        });
       });
     });
   }, []);
 
   const link = apiHost + "/webhook/" + webHookId;
-  const card = requests.find((e) => e.id === selectedRequestId) ?? requests[0];
+  const requestEvent =
+    requests.find((e) => e.id === selectedRequestId) ?? requests[0];
 
   return (
     <Box sx={{ display: "flex" }}>
@@ -86,49 +90,66 @@ export default function ClippedDrawer() {
         }}
       >
         <Toolbar />
-        <Box sx={{ overflow: "auto" }}>
-          <Typography align="center">
-            {"Awaiting events... "}
-            <CircularProgress size={12} />
-          </Typography>
+        <Box sx={{ overflow: "auto" }} style={{ padding: theme.spacing(1) }}>
+          {requests.length === 0 ? (
+            <Typography align="center">
+              {"Waiting for first event... "}
+              <CircularProgress size={12} />
+            </Typography>
+          ) : null}
+
           {requests.map(({ time, id }, i) => {
             return (
-     
-                <Button variant="outlined" fullWidth
-                  style={{ height: 25, alignItems: "center" }}
-                  onClick={() => {
-                    setSelectedRequestId(id);
-                  }}
-                >
-                  {new Date(time).toLocaleString()}
-                </Button>
-  
+              <Button
+                variant="outlined"
+                fullWidth
+                style={{ height: 25, alignItems: "center" }}
+                onClick={() => {
+                  setSelectedRequestId(id);
+                }}
+                color={i === 0 ? "success" : "primary"}
+              >
+                {new Date(time).toLocaleString()}
+              </Button>
             );
           })}
         </Box>
       </Drawer>
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+      <Box sx={{ flexGrow: 1, p: 3 }}>
         <Toolbar />
-
-        <Typography paragraph align="center" boxShadow={2}>
+        <Typography
+          paragraph
+          align="center"
+          boxShadow={2}
+          color={theme.palette.grey[700]}
+        >
+          <Typography component="span" color={theme.palette.text.primary} fontSize={12}>
+            {"Use this URL > "}
+          </Typography>
           {link}
-          <CopyToClipboardButton webHookId={link} />
+          <CopyToClipboardButton message={link} />
         </Typography>
+
         <Grid container spacing={1}>
-          {card ? (
+          {requestEvent ? (
             <>
-              <Grid item xs={6}>
+              <Grid item xs={5}>
                 Body
+                <CopyToClipboardButton message={requestEvent.body} />
                 <JsonFormatter
-                  json={card.body}
+                  json={requestEvent.body}
                   tabWith={4}
                   jsonStyle={jsonStyle}
                 />
               </Grid>
+              <Grid item xs={1}>
+                <Divider orientation="vertical" />
+              </Grid>
               <Grid item xs={6}>
                 Headers
+                <CopyToClipboardButton message={requestEvent.headers} />
                 <JsonFormatter
-                  json={card.headers}
+                  json={requestEvent.headers}
                   tabWith={4}
                   jsonStyle={jsonStyle}
                 />
@@ -141,21 +162,21 @@ export default function ClippedDrawer() {
   );
 }
 
-const CopyToClipboardButton = (props: { webHookId: string }) => {
+const CopyToClipboardButton = (props: { message: string }) => {
   const [open, setOpen] = React.useState(false);
 
   const handleClick = () => {
     setOpen(true);
-    navigator.clipboard.writeText(props.webHookId);
+    navigator.clipboard.writeText(props.message);
   };
 
   return (
     <>
       <IconButton onClick={handleClick} color="primary">
-        <ShareIcon />
+        <ContentCopyIcon />
       </IconButton>
       <Snackbar
-        message="Copied to clibboard"
+        message="Copied to clipboard"
         anchorOrigin={{ vertical: "top", horizontal: "center" }}
         autoHideDuration={2000}
         onClose={() => setOpen(false)}
